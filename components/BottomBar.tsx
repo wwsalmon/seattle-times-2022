7a38da46141a7328ebe35756a1554a59529438da
Samson Zhang <wwsamson@yahoo.com>
2022-11-03 21:39:56 -0700

    add covid tracker page

---
+++ components/BottomBar.tsx
@@ -0,0 +1,35 @@
+import {useEffect, useState} from "react";
+import classNames from "classnames";
+import {useRouter} from "next/router";
+
+export default function BottomBar({inline}: {inline?: boolean}) {
+    const router = useRouter();
+    const [readList, setReadList] = useState<string[]>([]);
+
+    useEffect(() => {
+        let localReadList = JSON.parse(window.localStorage.getItem("readList") || "[]");
+        const thisRoute = router.route.substring(1);
+        if (["covid", "pulitzers", "admissions", "ice"].includes(thisRoute) && !localReadList.includes(thisRoute)) {
+            localReadList.push(thisRoute);
+        }
+        setReadList(localReadList);
+    }, []);
+
+    useEffect(() => {
+        window.localStorage.setItem("readList", JSON.stringify(readList));
+    }, [readList]);
+
+    return (
+        <div className={classNames(!inline && "p-4 fixed w-full bottom-0 bg-white left-0 z-5")}>
+            <div className={classNames(!inline && "max-w-2xl mx-auto px-4")}>
+                <p>You've read {readList.length} of 4 scenarios.</p>
+                <div className="w-full h-4 bg-zinc-200 my-2 relative">
+                    <div className="absolute h-full bg-green-500 w-1/4"></div>
+                </div>
+                {inline && (
+                    <p className="text-right">Click through all four stories below to unlock my final message.</p>
+                )}
+            </div>
+        </div>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,26 @@
+import BottomBar from "../components/BottomBar";
 import {useEffect, useState} from "react";
+import classNames from "classnames";
+import Link from "next/link";
+
+const items = [
+    {
+        title: "COVID data that faculty relied on in the absence of admin reporting",
+        id: "covid",
+    },
+    {
+        title: "A first-of-its-kind investigation into journalism’s top awards",
+        id: "pulitzers",
+    },
+    {
+        title: "Explaining a COVID-driven decrease in student diversity",
+        id: "admissions",
+    },
+    {
+        title: "Insights from 10 years of anti-ICE organizing",
+        id: "ice",
+    },
+]
 
 const TOD = (() => {
     const currHour = new Date().getHours();
@@ -7,47 +29,43 @@ const TOD = (() => {
     return "evening";
 })();
 
-function Card({index, title}: {index: number, title: string}) {
+function Card({index, title, read, id}: {index: number, title: string, read: boolean, id: string}) {
     return (
-        <div className="bg-zinc-800 p-4 rounded-md mb-4 card flex flex-col justify-between">
-            <h2 className="font-meta text-3xl font-bold mb-8">{index}.</h2>
-            <p className="text-3xl">{title}</p>
-        </div>
+        <Link href={`/${id}`}>
+            <a className={classNames(read ? "bg-zinc-400 hover:scale-[1.02] hover:shadow-lg" : "bg-zinc-800 hover:scale-105 hover:shadow-xl", "p-4 rounded-md mb-4 flex flex-col justify-between relative card transform transition")}>
+                {read && (
+                    <p className="absolute top-4 right-4 font-bold uppercase tracking-widest">READ</p>
+                )}
+                <h2 className="font-meta text-3xl font-bold mb-8">{index}.</h2>
+                <p className="text-3xl">{title}</p>
+            </a>
+        </Link>
     )
 }
 
 export default function Home() {
-    const [numRead, setNumRead] = useState<number>(0);
+    const [readList, setReadList] = useState<string[]>([]);
 
     useEffect(() => {
-        setNumRead(JSON.parse(window.localStorage.getItem("numRead")))
+        let localReadList = JSON.parse(window.localStorage.getItem("readList"));
+        setReadList(localReadList);
     }, []);
-
-    useEffect(() => {
-        window.localStorage.setItem("numRead", numRead.toString());
-    }, [numRead]);
 
     return (
         <div className="mx-auto max-w-2xl px-4">
-            <div className="text-center py-16 text-xl max-w-lg mx-auto">
+            <div className="text-center my-16 text-xl max-w-lg mx-auto">
                 <h1 className="font-meta font-medium text-4xl">Good {TOD}, Seattle Times.</h1>
-                <p className="my-4">I thought an interactive portfolio would be appropriate for an interactives internship application.</p>
                 <p className="my-4">Click through the four scenarios below to learn how I’ve <b>served communities as a journalist and interactives developer,</b> and why I want to keep doing it at The Seattle Times this summer.</p>
-                <div className="flex items-center justify-center">
+                <div className="flex items-center justify-center my-16">
                     <img src="/profile.jpg" alt="Profile picture of Samson Zhang" className="w-12 h-12 rounded-full mr-6"/>
                     <p><span className="font-meta">By </span><a href="twitter.com/wwsalmon" className="text-linkBlue">Samson Zhang</a></p>
                 </div>
             </div>
-            <div className="my-8">
-                <p>You've read {numRead} of 4 scenarios.</p>
-                <div className="w-full h-4 bg-zinc-200 my-2"></div>
-                <p className="text-right">Click through all four stories below to unlock my final message.</p>
-            </div>
+            <BottomBar inline={true}/>
             <div className="grid grid-cols-2 gap-x-4 text-white my-24">
-                <Card index={1} title="COVID data that faculty relied on in the absence of admin reporting"/>
-                <Card index={2} title="A first-of-its-kind investigation into journalism’s top awards"/>
-                <Card index={3} title="Explaining a COVID-driven decrease in student diversity"/>
-                <Card index={4} title="Insights from 10 years of anti-ICE organizing"/>
+                {items.map((d, i) => (
+                    <Card index={i + 1} title={d.title} read={readList.includes(d.id)} key={d.id} id={d.id}/>
+                ))}
             </div>
         </div>
     );
